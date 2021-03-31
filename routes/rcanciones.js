2c5a2337cb51b9b83165f56ca039ffcc92cc4163
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-04-01 01:43:08 +0200

    SDI-2021-410-7.5-Vistas y motores de plantillas.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,19 +1,16 @@
-module.exports = function(app) {
-    app.get("/canciones", function(req, res) {
-        let respuesta = "";
-        if (req.query.nombre != null)
-            respuesta += 'Nombre: ' + req.query.nombre + '<br>';
-
-        if (typeof (req.query.autor) != "undefined")
-            respuesta += 'Autor: ' + req.query.autor;
-
-        res.send(respuesta);
-    });
+module.exports = function(app, swig) {
 
     app.get('/suma', function(req, res) {
         let respuesta =  parseInt(req.query.num1) + parseInt(req.query.num2);
 
         res.send((String(respuesta)));
+    });
+
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
     });
 
     app.get('/canciones/:id', function(req, res) {
@@ -32,6 +29,28 @@ module.exports = function(app) {
         + "precio: " + req.body.precio);
     });
 
+    app.get("/canciones", function(req, res) {
+
+        let canciones = [{
+            "nombre": "Blank space",
+            "precio": "1.2"
+        }, {
+            "nombre": "See you again",
+            "precio": "1.3"
+        }, {
+            "nombre" : "Uptown Funk",
+            "precio" : "1.1"
+
+        }];
+
+        let respuesta = swig.renderFile('views/btienda.html', {
+            vendedor : 'Tienda de canciones',
+            canciones : canciones
+
+        });
+
+        res.send(respuesta);
+    });
 
 
 };
