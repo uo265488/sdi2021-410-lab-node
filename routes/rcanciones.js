b0f111587f102c358bbb6298c7eaad8f13f94de8
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-04-04 19:47:40 +0200

    SDI-2021-410-8.1-Acceso a datos de Mongo desde Node.js.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,4 +1,4 @@
-module.exports = function(app, swig) {
+module.exports = function(app, swig, mongo) {
 
     app.get('/suma', function(req, res) {
         let respuesta =  parseInt(req.query.num1) + parseInt(req.query.num2);
@@ -24,9 +24,27 @@ module.exports = function(app, swig) {
     });
 
     app.post("/cancion", function(req, res) {
-        res.send("Cancion agregada: " + req.body.nombre + "<br>"
-        + "genero: " + req.body.genero + "<br>"
-        + "precio: " + req.body.precio);
+        let cancion = {
+            nombre : req.body.nombre,
+            genero : req.body.genero,
+            precio : req.body.precio
+        }
+        // Conectarse
+        mongo.MongoClient.connect(app.get('db'), function(err, db) {
+            if (err) {
+                res.send("Error de conexión: " + err);
+            } else {
+                let collection = db.collection('canciones');
+                collection.insertOne(cancion, function(err, result) {
+                    if (err) {
+                        res.send("Error al insertar " + err);
+                    } else {
+                        res.send("Agregada id: "+ result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
     });
 
     app.get("/canciones", function(req, res) {
